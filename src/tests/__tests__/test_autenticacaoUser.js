f040edf35afa0df9316c1c6183c0b7be6d3a67a7
Hellen159 <202016480@aluno.unb.br>
2022-08-21 20:27:30 -0400

    69-testes de createUser

--- src/tests/__tests__/test_autenticacaoUser.js
+++ src/tests/__tests__/test_autenticacaoUser.js
@@ -1,6 +1,8 @@
+// import User from "schemas/User";
 import supertest from "supertest";
 import app from "../../app";
 import { mongoDB } from "../fixtures";
+import bcrypt from "bcrypt";
 
 const NAME = "Will";
 const EMAIL = "will@gmail.com";
@@ -19,14 +21,48 @@ beforeAll(async () => {
       email: EMAIL,
       password: PASSWORD,
     });
+  delete globalResponse.password;
 });
 
 afterAll((done) => {
   mongoDB.disconnect(done);
 });
 
-describe.skip("post new user", () => {
+describe("post new user", () => {
   test("testa o endpoint newUser", async () => {
+    expect(globalResponse.status).toBe(200);
+    expect(globalResponse.body).toEqual({
+      name: NAME,
+      email: EMAIL,
+      ...globalResponse.body,
+    });
+  });
+  test("testa o endpoint newUser se der errado", async () => {
+    const response = await supertest(app).post("/newUser").send({
+      email: EMAIL,
+    });
+    expect(response.status).toBe(500);
+  });
+});
+// describe.skip("get all user", () => {
+//   test("testa o endpoint newUser", async () => {
+//     expect(globalResponse.status).toBe(200);
+//     expect(globalResponse.body).toEqual({
+//       nome: NAME,
+//       email: EMAIL,
+//       password: PASSWORD,
+//       ...globalResponse.body,
+//     });
+//   });
+//   test("testa o endpoint allUser se der errado", async () => {
+//     const response = await supertest(app)
+//       .get("/user")
+//       .set("Content-Type", "application/json");
+//   });
+//   expect(response.status).toBe(500);
+// });
+describe.skip("post login", () => {
+  test("testa o endpoint login", async () => {
     expect(globalResponse.status).toBe(200);
     expect(globalResponse.body).toEqual({
       email: EMAIL,
@@ -36,7 +72,7 @@ describe.skip("post new user", () => {
   });
   test("testa o endpoint newUser se der errado", async () => {
     const response = await supertest(app)
-      .post("/newUser")
+      .post("/login")
       .set("Content-Type", "application/json")
       .send({
         email: EMAIL,
