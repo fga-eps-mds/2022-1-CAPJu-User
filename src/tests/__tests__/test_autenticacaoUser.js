27e282f6afe00582ae30283249078f2505ca716a
Hellen159 <202016480@aluno.unb.br>
2022-08-22 22:34:01 -0400

    69-teste de login e allUser

--- src/tests/__tests__/test_autenticacaoUser.js
+++ src/tests/__tests__/test_autenticacaoUser.js
@@ -2,11 +2,20 @@
 import supertest from "supertest";
 import app from "../../app";
 import { mongoDB } from "../fixtures";
-import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+const generateToken = (ID) => {
+  return jwt.sign({ ID }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 const NAME = "Will";
 const EMAIL = "will@gmail.com";
 const PASSWORD = "will123";
+const ID = "63042d727576f01df37ad552";
+const TOKEN = generateToken(ID);
+const PASSWORDCRIPTO =
+  "$2b$10$vtXewtwsqi.8/ySCn3VnhuJWpnDhJGt7JtAVnsuA1EEegNVdy.x7C";
 
 let globalResponse;
 
@@ -19,21 +28,22 @@ beforeAll(async () => {
     .send({
       name: NAME,
       email: EMAIL,
-      password: PASSWORD,
+      password: PASSWORDCRIPTO,
     });
-  delete globalResponse.password;
 });
 
 afterAll((done) => {
   mongoDB.disconnect(done);
 });
-
+//createUser---------------------------------
 describe("post new user", () => {
   test("testa o endpoint newUser", async () => {
     expect(globalResponse.status).toBe(200);
     expect(globalResponse.body).toEqual({
+      _id: ID,
       name: NAME,
       email: EMAIL,
+      token: TOKEN,
       ...globalResponse.body,
     });
   });
@@ -44,39 +54,52 @@ describe("post new user", () => {
     expect(response.status).toBe(500);
   });
 });
-// describe.skip("get all user", () => {
-//   test("testa o endpoint newUser", async () => {
-//     expect(globalResponse.status).toBe(200);
-//     expect(globalResponse.body).toEqual({
-//       nome: NAME,
-//       email: EMAIL,
-//       password: PASSWORD,
-//       ...globalResponse.body,
-//     });
-//   });
-//   test("testa o endpoint allUser se der errado", async () => {
-//     const response = await supertest(app)
-//       .get("/user")
-//       .set("Content-Type", "application/json");
-//   });
-//   expect(response.status).toBe(500);
-// });
-describe.skip("post login", () => {
+//Login----------------------------------------------------OKOK
+describe("post login", () => {
   test("testa o endpoint login", async () => {
     expect(globalResponse.status).toBe(200);
     expect(globalResponse.body).toEqual({
+      _id: ID,
+      name: NAME,
       email: EMAIL,
-      password: PASSWORD,
+      token: TOKEN,
       ...globalResponse.body,
     });
   });
-  test("testa o endpoint newUser se der errado", async () => {
-    const response = await supertest(app)
-      .post("/login")
-      .set("Content-Type", "application/json")
-      .send({
-        email: EMAIL,
-      });
+  test("testa o endpoint login se der errado", async () => {
+    const response = await supertest(app).get("/login").send({
+      name: NAME,
+    });
+    expect(response.status).toBe(404);
+  });
+});
+//allUser----------------------------------------------------------------OKOK
+describe("all user", () => {
+  test("testa o endpoint allUser", async () => {
+    expect(globalResponse.status).toBe(200);
+    expect(globalResponse.body).toEqual({
+      _id: ID,
+      name: NAME,
+      email: EMAIL,
+      token: TOKEN,
+      ...globalResponse.body,
+    });
+  });
+  test("testa o endpoint allUser se der errado", async () => {
+    const response = await supertest(app).get("/allUser");
+    expect(response.status).toBe(401);
+  });
+});
+//user--------------------------------------------------------
+describe("user", () => {
+  test("testa o endpoint user", async () => {
+    expect(globalResponse.status).toBe(200);
+    expect(globalResponse.body).toEqual({
+      name: NAME,
+    });
+  });
+  test("testa o endpoint user se der errado", async () => {
+    const response = await supertest(app).get("/user");
     expect(response.status).toBe(500);
   });
 });
