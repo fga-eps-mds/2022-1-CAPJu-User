b85ba606e397d17b5b73878034bfd54c2af6cdc4
Hellen159 <202016480@aluno.unb.br>
2022-09-08 19:56:04 -0400

    72- adicionando rota de edit password
    Signed-off-by: Hellen159 <202016480@aluno.unb.br>
    Co-authored-by: githubviramundo <luciano.abs.machado@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,10 @@ const routes = Router();
 
 routes.get("/user", UserController.user);
 routes.post("/newUser", UserController.createUser);
-routes.get("/allUser", UserController.allUser);
+routes.get("/allUser", protect, UserController.allUser);
 routes.post("/login", UserController.login);
 routes.post("/requestRecovery", UserController.requestRecoveryMail);
 routes.post("/updatePassword", UserController.updatePassword);
-routes.put("/updateUser/:id", UserController.updateUser);
+routes.put("/updateUser/:id", protect, UserController.updateUser);
+routes.put("/updateUser/:id", protect, UserController.updatePassword);
 export default routes;
