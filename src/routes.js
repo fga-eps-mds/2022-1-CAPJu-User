24798ed75f55a1ff75bb9aaff6a10d033345dea2
Hellen159 <202016480@aluno.unb.br>
2022-09-17 23:36:38 -0400

    72- Arrumando rotas
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>
    Co-authored-by: githubviramundo <luciano.abs.machado@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const routes = Router();
 
 routes.get("/user", UserController.user);
 routes.post("/newUser", UserController.createUser);
-routes.get("/allUser", UserController.allUser);
+routes.get("/allUser", protect, UserController.allUser);
 routes.post("/login", UserController.login);
 routes.post("/requestRecovery", UserController.requestRecoveryMail);
 routes.post("/updatePassword", UserController.updatePassword);
